5746d138416b8d60bd9955cf5099f6a5709b70b2
Etwashoeren <sungmin0609@nate.com>
2024-03-26 21:54:53 +0900

    update

---
+++ Data_Structure/1940.py
@@ -0,0 +1,19 @@
+N = int(input())
+M = int(input())
+A = list(map(int, input().split()))
+A.sort()
+i = 1
+j = N - 1
+count = 0
+
+while i < j:
+    if A[i] + A[j] < M:
+        i += 1
+    elif A[i] + A[j] > M:
+        j -= 1
+    else:
+        count += 1
+        i += 1
+        j -= 1
+
+print(count)
--- Data_Structure/B2018.py
+++ Data_Structure/B2018.py
@@ -7,11 +7,11 @@ sum = 1
 while end_index != n:
     if sum == n:
         count += 1
-        end_index += 1
-        sum += end_index
+        end_index += 1 # end_index를 오른쪽으로 한 칸 이동
+        sum += end_index # 연속된 자연수 범위를 한 칸 더 확장
     elif sum > n:
-        sum -= start_index
-        start_index += 1
+        sum -= start_index # 배열의 왼쪽 값 삭제
+        start_index += 1 # start_index를 오른쪽으로 한 칸 이동
     else:
         end_index += 1
         sum += end_index
