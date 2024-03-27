566930ae610521ea19f6914e0e27b2cafae4c5b4
Etwashoeren <sungmin0609@nate.com>
2024-03-27 14:36:36 +0900

    평균 구하기

--- Data_Structure/B1546.py
+++ Data_Structure/B1546.py
@@ -1,3 +1,4 @@
+# 평균 구하기
 n = input()                              # n에 과목의 수 입력
 mylist = list(map(int, input().split())) # mylist에 점수 정보 저장
 mymax = max(mylist)                      # mymax에 mylist 정보 중 최댓값 저장
